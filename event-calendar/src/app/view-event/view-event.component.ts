fa024ed2ce27fa3cd29090de8fa03355bce2a554
Joshua Kristanto <joshuakristanto68@gmail.com>
2021-11-23 04:13:58 -0800

    Updates to Data Collection.

--- event-calendar/src/app/view-event/view-event.component.ts
+++ event-calendar/src/app/view-event/view-event.component.ts
@@ -3,7 +3,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { startOfDay } from 'date-fns';
 import { CalendarView, CalendarEvent } from 'angular-calendar';
 import { AddEventComponent } from '../add-event/add-event.component';
-import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
@@ -17,21 +17,21 @@ export class ViewEventComponent implements OnInit {
   @Input() my_modal_content: any;
   @Input() my_modal_comment: any;
   @Input() date: any;
-  @Input()array: Array<any> =[];
+  @Input() array: Array<any> = [];
   @Output() update = new EventEmitter<any>();
-  modalOptions:NgbModalOptions;
+  modalOptions: NgbModalOptions;
 
-  constructor(public activeModal: NgbActiveModal, private modalService :NgbModal, private http: HttpClient) {
+  constructor(public activeModal: NgbActiveModal, private modalService: NgbModal, private http: HttpClient) {
 
     this.modalOptions = {
-      backdrop:'static',
-      backdropClass:'customBackdrop'
+      backdrop: 'static',
+      backdropClass: 'customBackdrop'
 
     }
 
   }
 
-  
+
 
   // receiveMessage($event : any) {
   //   if($event === "Update")
@@ -41,134 +41,139 @@ export class ViewEventComponent implements OnInit {
   //   }
   // }
   ngOnInit() {
-        
+
     const param = new HttpParams()
-    .append('date', this.date.toISOString());
+      .append('date', this.date.toISOString());
 
 
-const body=JSON.stringify("");
-console.log(`Bearer ` +  localStorage.getItem('jwt'));
-const header = new HttpHeaders()
-  .append(
-    'Content-Type',
-    'application/json'
-  )
-  .append('Authorization', `Bearer ` + localStorage.getItem('jwt'));
+    const body = JSON.stringify("");
+    console.log(`Bearer ` + localStorage.getItem('jwt'));
+    const header = new HttpHeaders()
+      .append(
+        'Content-Type',
+        'application/json'
+      )
+      .append('Authorization', `Bearer ` + localStorage.getItem('jwt'));
 
-this.http.get<any>("https://localhost:44382/Events/Day", ({headers: header, params:param}) ).subscribe(result => {
+    this.http.get<any>("https://localhost:44382/Events/Day", ({ headers: header, params: param })).subscribe(result => {
 
-console.log(result.toString())
-// var output = JSON.parse(result);
-console.log(result[0]['items']);
-// console.log(result[0]['eventContents']['comment']);
-    this.my_modal_content = result[0]['items']['title'];
-    this.my_modal_comment = result[0]['items']['comment'];
-    //  this.array= [{'title':result[0]['title'], 'comment': result[0]['comment'] }]; 
-    for (var item in result['items']){
-      this.array.push(result['item'][item]);
-    } 
+      console.log("Day", result.toString())
+      // var output = JSON.parse(result);
+      console.log(result[0]);
+      // console.log(result[0]['eventContents']['comment']);
+      this.my_modal_content = result[0]['items']['title'];
+      this.my_modal_comment = result[0]['items']['comment'];
+      //  this.array= [{'title':result[0]['title'], 'comment': result[0]['comment'] }]; 
+      for (var item in result[0]['items']) {
+        this.array.push(result[0]['items'][item]);
+      }
 
-   }, error => console.error(error));
-
-
+    }, error => console.error(error));
 
 
-   
+
+
+
   }
-  openAddEvent(  title: any, date:any) {
+  openAddEvent(title: any, date: any) {
     const modalRef = this.modalService.open(AddEventComponent);
     modalRef.componentInstance.my_modal_title = title;
     modalRef.componentInstance.my_modal_content = "EVENT";
     console.log("DATE" + ":" + date);
     modalRef.componentInstance.date = date;
     // this.activeModal.close('Close click');
-    modalRef.componentInstance.update.subscribe((event:any)=>
-    {
+    modalRef.componentInstance.update.subscribe((event: any) => {
       // setTimeout(() => {  console.log("World!"); }, 2000);
       this.updateEvent();
-      this.update.emit({update: "Update"});
+      this.update.emit({ update: "Update" });
     })
     // modalRef.componentInstance["Update"].subscribe((event: any) => {
     //   console.log(" EVENT" +event) //< you now have access to the event that was emitted, to pass to your grandfather component.
     //  });
 
-  
+
   }
 
-  updateEvent(){
+  updateEvent() {
     const param = new HttpParams()
-    .append('date', this.date.toISOString());
+      .append('date', this.date.toISOString());
 
-const body=JSON.stringify("");
+    const body = JSON.stringify("");
 
-const header = new HttpHeaders()
-.append(
-  'Content-Type',
-  'application/json'
-)
-.append('Authorization', `Bearer ` + localStorage.getItem('jwt'));
-this.http.get<any>("https://localhost:44382/Events/Day", ({headers: header, params:param}) ).subscribe(result => {
+    const header = new HttpHeaders()
+      .append(
+        'Content-Type',
+        'application/json'
+      )
+      .append('Authorization', `Bearer ` + localStorage.getItem('jwt'));
+    this.http.get<any>("https://localhost:44382/Events/Day", ({ headers: header, params: param })).subscribe(result => {
 
-console.log(result.toString())
-// var output = JSON.parse(result);
-console.log(result[0]['title']);
-// console.log(result[0]['eventContents']['comment']);
-this.my_modal_content = result[0]['title'];
-this.my_modal_comment = result[0]['comment']
-//  this.array= [{'title':result[0]['title'], 'comment': result[0]['comment'] }]; 
-for (var item in result){
-  this.array.push(result[item]);
-} 
-   }, error => console.error(error));
+      console.log("Day", result.toString())
+      // var output = JSON.parse(result);
+      console.log(result[0]);
+      // console.log(result[0]['eventContents']['comment']);
+      this.my_modal_content = result[0]['items']['title'];
+      this.my_modal_comment = result[0]['items']['comment'];
+      this.array = [];
+      //  this.array= [{'title':result[0]['title'], 'comment': result[0]['comment'] }]; 
+      for (var item in result[0]['items']) {
+        this.array.push(result[0]['items'][item]);
+      }
+
+    }, error => console.error(error));
+
+
+
+
 
   }
 
-  deleteEvent( date:Date){
+  deleteEvent(date: Date) {
     // this.http.post<Event>("https://localhost:44382/Events/Add", params:({ date: this.date ,title: localTitle , comment:localComment })).subscribe(result => {
-      
+
     //   }, error => console.error(error));
-  
+
     const param = new HttpParams()
-        .append('date', date.toISOString());
-  
-    const body=JSON.stringify("");
-  
+      .append('date', date.toISOString());
+
+    const body = JSON.stringify("");
+
     let token = localStorage.getItem('jwt');
     const header = new HttpHeaders()
-    .append(
-      'Content-Type',
-      'application/json'
-    )
-    .append ('Authorization', `Bearer ${token}`);
-    this.http.post<Event>("https://localhost:44382/Events/Delete", body,({headers: header, params:param}) ).subscribe(result => {
+      .append(
+        'Content-Type',
+        'application/json'
+      )
+      .append('Authorization', `Bearer ${token}`);
+    this.http.post<Event>("https://localhost:44382/Events/Delete", body, ({ headers: header, params: param })).subscribe(result => {
       this.updateEvent();
-      this.update.emit({update: "Update"});
-      
-       }, error => console.error(error));
-  
-  this.activeModal.close("Close click");
-    
-  
+      this.update.emit({ update: "Update" });
+
+    }, error => console.error(error));
+
+    this.activeModal.close("Close click");
+
+
   }
 
 
 
- 
 
 
- 
+
+
 
 }
 
-interface Event{
-  created : Date;
+interface Event {
+  created: Date;
   year: number;
   month: number;
   day: number;
   commentedCreated: Date;
   eventsContent: EventContents;
 }
-interface EventContents{
+interface EventContents {
   commentedCreated: Date;
   title: string;
   comment: string;
