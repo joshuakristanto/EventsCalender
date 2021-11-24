cf8b0778b24eda5773f6617b68c26ccf56b2da21
Joshua Kristanto <joshuakristanto68@gmail.com>
2021-11-23 21:47:07 -0800

    First change to VSAzure

---
+++ EventCalendarIntegrated/EventCalendar/EventCalendar/ClientApp/src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { LoginComponent } from './login/login.component';
+import { AddEventComponent } from './add-event/add-event.component';
+import { ViewEventComponent } from './view-event/view-event.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateAccountComponent } from './create-account/create-account.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    LoginComponent,
+    AddEventComponent,
+    ViewEventComponent,
+    CreateAccountComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    FormsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'createAccount', component: CreateAccountComponent },
+    ]),
+    BrowserAnimationsModule
+  ],
+  entryComponents: [ ViewEventComponent,  LoginComponent,
+    AddEventComponent, CreateAccountComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
