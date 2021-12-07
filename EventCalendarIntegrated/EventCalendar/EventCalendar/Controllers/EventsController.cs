42a9e0f9906310c081e1b9fdc457d7acebbb8bf4
Joshua Kristanto <joshuakristanto68@gmail.com>
2021-12-07 07:07:33 -0800

    Code Cleanup

--- EventCalendarIntegrated/EventCalendar/EventCalendar/Controllers/EventsController.cs
+++ EventCalendarIntegrated/EventCalendar/EventCalendar/Controllers/EventsController.cs
@@ -44,28 +44,7 @@ namespace EventCalendar.Controllers
         public IEnumerable GetMonthEvents( int month, int year)
         {
            
-            /*using (var db = _eventData)
-            {
-                var localEvent = db.Events;
-                var localEventContent = db.EventsContents;
-                //Switch Include to Select. 
-
-                // var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).Include(c=>c.EventsContents).ToList();
-                  var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).SelectMany(c => c.Items, (c,i) => new { c.Created,i.Title } ).ToList();
-              //  var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).Select(c => new { c.Created, EventContents =  c.Items.Select(k => k.Title)}).ToList();
-                // var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).ToList();
-                foreach (var events in localEventComplete)
-                {
-                    Console.WriteLine("Order: order.Created");
-                    
-                    //var comm =localComment.Find(events.EventsContents);
-                    
-
-                    yield return events ;
-                }
-
-            }
-            */
+            
 
             return _calendarDb.GetMonthEvents(month, year);
 
@@ -79,26 +58,7 @@ namespace EventCalendar.Controllers
         [HttpGet]
         public IEnumerable GetMonthEventContents(int month, int year)
         {
-            using (var db = _eventData)
-            {
-                var localEvent = db.Events;
-                var localEventContent = db.EventsContents;
-                //Switch Include to Select. 
-
-                //  var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).Include(c=>c.EventsContents).ToList();
-                var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).SelectMany(c => c.Items, (c, i) => new { c.Created, i.Title ,i.Comment}).ToList();
-                // var localEventComplete = localEvent.Where(c => c.Month == month && c.Year == year).ToList();
-                foreach (var events in localEventComplete)
-                {
-                    Console.WriteLine("Order: order.Created");
-
-                    //var comm =localComment.Find(events.EventsContents);
-
-
-                    yield return events;
-                }
-
-            }
+            return _calendarDb.GetMonthEventContents(month, year);
 
         }
 
@@ -108,12 +68,7 @@ namespace EventCalendar.Controllers
         [HttpGet]
         public IEnumerable GetMonthDayEvents( DateTime date)
         {
-            var db = _eventData;
-
-
-            // var results = db.Events.Where(p => p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month && p.Created.Value.Date.Year == date.Year).SelectMany(c => c.Items, (c,i) => new { c.Created, i.Title, i.Comment, i.Id });
-            var results = db.Events.Where(p => p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month && p.Created.Value.Date.Year == date.Year).Select(c => new { c.Created, items = c.Items.Select(c => new { c.Title, c.Comment, c.Id }) });
-            return results;
+            return _calendarDb.GetMonthDayEvents(date);
         }
         // [Authorize]
         [Route("Test")]
@@ -134,78 +89,11 @@ namespace EventCalendar.Controllers
         [HttpPost]
         public IActionResult AddEvent(DateTime date, string title, string comment)
         {
-            using (var db = _eventData)
-            {
-                var ticks = new DateTime(2016, 1, 1).Ticks;
-                var ans = DateTime.Now.Ticks - ticks;
-                var uniqueId = ans.ToString("x");
-
-
-                
-
-               
-                var day = db.Events;
-                ICollection<EventsContents>[] dbContent = day.Where(c => c.Created == date).Select(c => c.Items).ToArray();
-                Events [] eventLocal = day.Where(c => c.Created == date).ToArray();
-              
-                if (eventLocal.Length >0)
-                {
-                    var eventContents = new EventsContents()
-                    {
-                        Id = uniqueId,
-                        //   CommentCreated = date,
-                        Comment = comment,
-                        Title = title,
-                        EventId = uniqueId,
-                        Events = eventLocal[0],
-
-                    };
-                    dbContent[0].Add(eventContents);
-                    eventLocal[0].Items = dbContent[0];
-
-                    db.Events.Update(eventLocal[0]);
-                    db.EventsContents.Add(eventContents);
-                }
-                else
-                {
-                    var events = new Events()
-                    {
-
-                        Created = date,
-                        Year = date.Year,
-                        Month = date.Month,
-                        Day = date.Day,
-                        Items = new List<EventsContents>(),
-                        EventId = uniqueId,
-
-                    }; 
-                    var eventContents = new EventsContents()
-                    {
-                        Id = uniqueId,
-                        //   CommentCreated = date,
-                        Comment = comment,
-                        Title = title,
-                        EventId = uniqueId,
-                        Events = events,
-
-                    };
-                    events.Items.Add(eventContents);
-                    db.Events.AddAsync(events);
-                    db.EventsContents.Add(eventContents);
-
-                }
-               
-                // events.id.Add(uniqueId);
-               
-              
-              //  db.Events.AddAsync(events);
-              db.SaveChanges();
-
-
-            };
             
 
-           // var db = new CalendarEventData();
+            _calendarDb.AddEvent(date, title, comment);
+            
+            
 
            return Ok();
         }
@@ -214,39 +102,7 @@ namespace EventCalendar.Controllers
         [HttpPost]
         public IActionResult DeleteEvent(DateTime date)
         {
-            using (var db = _eventData)
-            {
-
-                var results = db.Events.Where(p =>
-                    p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month &&
-                    p.Created.Value.Date.Year == date.Year).ToArray();
-                var resultsEventContents = db.Events.Where(p =>
-                    p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month &&
-                    p.Created.Value.Date.Year == date.Year).Select(c =>c.Items).ToArray();
-                /*
-                var resultsEventContent = db.EventsContents.Where(p =>
-                    p.CommentCreated.Value.Date.Day == date.Day && p.CommentCreated.Value.Date.Month == date.Month &&
-                    p.CommentCreated.Value.Date.Year == date.Year).ToArray();
-                */
-                //var eventContents = resultsEventContent[0];
-                var events = results[0];
-                //  db.EventsContents.Remove(eventContents);
-
-                foreach (var item in resultsEventContents[0])
-                {
-                    db.EventsContents.Remove(item);
-                }
-
-                db.Events.Remove(events);
-                db.SaveChanges();
-
-
-            };
-
-
-
-            // var db = new CalendarEventData();
-
+            _calendarDb.DeleteEvent(date);
 
             return Ok();
         }
@@ -257,46 +113,7 @@ namespace EventCalendar.Controllers
         [HttpPost]
         public async Task<IActionResult> DeleteEventItem(DateTime date, string id)
         {
-            using (var db = _eventData)
-            {
-
-                var results = db.Events.Where(p =>
-                    p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month &&
-                    p.Created.Value.Date.Year == date.Year).ToArray();
-                var resultsEventContents = db.Events.Where(p =>
-                    p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month &&
-                    p.Created.Value.Date.Year == date.Year).Select(c => c.Items).ToArray();
-                /*
-                var resultsEventContent = db.EventsContents.Where(p =>
-                    p.CommentCreated.Value.Date.Day == date.Day && p.CommentCreated.Value.Date.Month == date.Month &&
-                    p.CommentCreated.Value.Date.Year == date.Year).ToArray();
-                */
-                //var eventContents = resultsEventContent[0];
-                var events = results[0];
-                //  db.EventsContents.Remove(eventContents);
-
-                foreach (var item in resultsEventContents[0])
-                {
-                    if (item.Id == id)
-                    {
-                        db.EventsContents.Remove(item);
-                      //  resultsEventContents[0].Remove(item);
-                    }
-                   // db.EventsContents.Remove(item);
-                }
-
-                events.Items = resultsEventContents[0];
-               // db.Events.Update(events);
-                //db.Events.Remove(events);
-                
-                db.SaveChanges();
-
-
-            };
-
-
-
-            // var db = new CalendarEventData();
+            _calendarDb.DeleteEventItem( date,id);
 
 
             return Ok();
@@ -306,43 +123,7 @@ namespace EventCalendar.Controllers
         [HttpPost]
         public async Task<IActionResult> EditEventItem(DateTime date, string id, string title, string comment)
         {
-            using (var db = _eventData)
-            {
-
-                var results = db.Events.Where(p =>
-                    p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month &&
-                    p.Created.Value.Date.Year == date.Year).ToArray();
-                var resultsEventContents = db.Events.Where(p =>
-                    p.Created.Value.Date.Day == date.Day && p.Created.Value.Date.Month == date.Month &&
-                    p.Created.Value.Date.Year == date.Year).Select(c => c.Items).ToArray();
-              
-                var events = results[0];
-
-                
-
-                foreach (var item in resultsEventContents[0])
-                {
-
-                    if (item.Id == id)
-                    {
-                        var eventContents = db.EventsContents.Find(item.Id);
-                        eventContents.Comment = comment;
-                        eventContents.Title = title;
-                    }
-                }
-
-                events.Items = resultsEventContents[0];
-              
-
-                db.SaveChanges();
-
-
-            };
-
-
-
-            // var db = new CalendarEventData();
-
+            _calendarDb.EditEventItem(date,id,title,comment);
 
             return Ok();
         }
