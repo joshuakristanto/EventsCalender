623a8b77fef8c82cc199e25f53a6beb51b62e572
Joshua Kristanto <joshuakristanto68@gmail.com>
2021-12-17 11:17:31 -0800

    Added more Tests.

---
+++ EventCalendarIntegrated/EventCalendar/EventCalendar/ViewModels/EventContentTestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCalendar.ViewModels
+{
+    public class EventContentTestModel
+    {
+        public string EventId { get; set; }
+
+        public string Title { get; set; }
+
+    }
+}
